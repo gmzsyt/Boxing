313d4fbb1ebb08a81b6d1ff0e01994b1620b6acb
Gamze Seyit <gmzzsyyt@gmail.com>
2021-12-26 18:17:11 +0300

    Add project files.

---
+++ ArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace ArratLis
+{
+    class Deneme
+    {
+        int x;
+
+        public Deneme(int x )
+        {
+            this.x = x;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList AList = new ArrayList();
+            AList.Add("GAMZE");
+            AList.Add("Ali");
+            AList.Add("Ayse");
+            object obj = AList;
+            Deneme d = new Deneme(5);
+            
+            AList.Add(d);
+
+            foreach(object i in AList)
+                Console.WriteLine("bakim " + (object)i);
+
+
+
+
+
+        }
+    }
+}
